1d53bc3bff5865ae4df72494954f9ae1843bca29
Jack Kibort <jkibort928@gmail.com>
2025-02-26 05:04:21 -0800

    compiler satisfied and lots of bugs gone

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,15 @@
+import System.IO
+import System.Environment (getArgs)
+import Control.Exception ( throw, Exception )
+import Control.Monad ( when, unless )
 
+-- Error handling
+import Data.Typeable ( Typeable )
+newtype Error = Error {errMsg :: String}
+    deriving (Show, Typeable)
+instance Exception Error
 
-
+-- Help message to be displayed
 helpMessage :: String
 helpMessage = "hi\n"
 
@@ -16,7 +25,7 @@ possibleLFlags = ["help", "port"]
 argFlags :: [Char]
 argFlags = ['p']
 argLFlags :: [String]
-argLFlags = "port"
+argLFlags = ["port"]
 
 -- Returns false if there is a flag that is not valid
 checkFlags :: [String] -> Bool
@@ -47,7 +56,7 @@ isLFlag _			= False
 isArgFlag :: String -> Bool
 isArgFlag "-"			= False
 isArgFlag ('-':c:[])	= c `elem` argFlags
-isArgFlag (c:[])		= c 'elem' argFlags -- idk if needed
+isArgFlag (c:[])		= c `elem` argFlags -- idk if needed
 isArgFlag _ 			= False
 
 -- Returns true if the string is a longflag that takes an argument
@@ -57,7 +66,7 @@ isArgLFlag str	= (removeDashes str) `elem` argLFlags
 
 -- Removes all dashes from the beginning of a string (strips a flag or longflag)
 removeDashes :: String -> String
-removeDashes (-:cs)	= removeDashes cs
+removeDashes ('-':cs)	= removeDashes cs
 removeDashes str	= str
 
 -- Parse a list of strings into a 5tuple of strings containing:
@@ -68,38 +77,37 @@ removeDashes str	= str
 --	List of lflagArgs
 parseArgs :: [String] -> ([String], [String], [String], [String], [String])
 parseArgs [] = throw (Error "Error: No arguments specified")
-parseArgs strs = helper strs [] [] [] [] [] true
+parseArgs strs = helper strs [] [] [] [] [] True
 	where
+		helper :: [String] -> [String] -> [String] -> [String] -> [String] -> [String] -> Bool -> ([String], [String], [String], [String], [String])
 		helper args argv fs lfs fargs lfargs processFlags = case args of
 			(s1:s2:ss)
-				| s1 == "--" 									-> helper s2:ss	argv 		fs						lfs							fargs		lfargs		False
-				| processFlags && isLFlag s1 && isArgLFlag s1	-> helper ss	argv 		fs						((removeDashes s1):lfs)		fargs		(s2:lfargs)	processFlags
-				| processFlags && isLFlag s1					-> helper s2:ss	argv 		fs						((removeDashes s1):lfs)		fargs		lfargs		processFlags
-				| processFlags && isFlag s1 && isArgFlag s1		-> helper ss	argv 		((removeDashes s1):fs)	lfs							(s2:fargs)	lfargs		processFlags
-				| processFlags && isFlag s1						-> helper s2:ss	argv 		((removeDashes s1):fs)	lfs							fargs		lfargs		processFlags
-				| otherwise										-> helper s2:ss	(s1:argv)	fs						lfs							fargs		lfargs		processFlags
+				| s1 == "--" 									-> helper (s2:ss)	argv 		fs						lfs							fargs		lfargs		False
+				| processFlags && isLFlag s1 && isArgLFlag s1	-> helper ss		argv 		fs						((removeDashes s1):lfs)		fargs		(s2:lfargs)	processFlags
+				| processFlags && isLFlag s1					-> helper (s2:ss)	argv 		fs						((removeDashes s1):lfs)		fargs		lfargs		processFlags
+				| processFlags && isFlag s1 && isArgFlag s1		-> helper ss		argv 		((removeDashes s1):fs)	lfs							(s2:fargs)	lfargs		processFlags
+				| processFlags && isFlag s1						-> helper (s2:ss)	argv 		((removeDashes s1):fs)	lfs							fargs		lfargs		processFlags
+				| otherwise										-> helper (s2:ss)	(s1:argv)	fs						lfs							fargs		lfargs		processFlags
 			(s1:s2:[])
-				| s == "--" 									-> 				((s2:argv),	fs, 					lfs,						fargs,		lfargs)
-				| processFlags && isLFlag s1 && isArgLFlag s1	-> 				(argv,		fs, 					((removeDashes s1):lfs),	fargs,		(s2:lfargs))
-				| processFlags && isLFlag s1					-> helper [s2]	argv,		fs	 					((removeDashes s1):lfs)		fargs		lfargs		processFlags
-				| processFlags && isFlag s1 && isArgFlag s1		->				(argv,		((removeDashes s1):fs),	lfs,						(s2:fargs),	lfargs)
-				| processFlags && isFlag s1						-> helper [s2]	argv,		((removeDashes s1):fs)	lfs							fargs		lfargs		processFlags
+				| s1 == "--" 									-> 					((s2:argv),	fs, 					lfs,						fargs,		lfargs)
+				| processFlags && isLFlag s1 && isArgLFlag s1	-> 					(argv,		fs, 					((removeDashes s1):lfs),	fargs,		(s2:lfargs))
+				| processFlags && isLFlag s1					-> helper [s2]		argv		fs	 					((removeDashes s1):lfs)		fargs		lfargs		processFlags
+				| processFlags && isFlag s1 && isArgFlag s1		->					(argv,		((removeDashes s1):fs),	lfs,						(s2:fargs),	lfargs)
+				| processFlags && isFlag s1						-> helper [s2]		argv		((removeDashes s1):fs)	lfs							fargs		lfargs		processFlags
 			(s:[])
-				| processFlags && isLFlag s && isArgLFlag s 	-> throw (Error "Error: A LongFlag is missing an argument!")
-				| processFlags && isLFlag s 					-> 				(argv,		fs,						(removeDashes s):lfs),		fargs,		lfargs
-				| processFlags && isFlag s && isArgFlag s 		-> throw (Error "Error: A Flag is missing an argument!")
-				| processFlags && isFlag s 						-> 				(argv,		((removeDashes s):fs),	lfs,						fargs,		lfargs
+				| processFlags && isLFlag s 					-> 					(argv,		fs,						((removeDashes s):lfs),		fargs,		lfargs)
+				| processFlags && isFlag s 						-> 					(argv,		((removeDashes s):fs),	lfs,						fargs,		lfargs)
 			[] -> (argv, fs, lfs, fargs, lfargs)
 			
 main :: IO ()
 main = do
 	args <- getArgs
-	let (argv, flags, longFlags) = parseArgs args
+	let (argv, flags, longFlags, flagArgs, longFlagArgs) = parseArgs args
 
-	if ('h' `elem` flags) || ("help" `elem` longFlags) then do
+	if ("h" `elem` flags) || ("help" `elem` longFlags) then do
 	        putStrLn helpMessage
 	else do
-	when (null argv)					$ throw (Error "Error: No arguments specified")
+		when (null argv)					$ throw (Error "Error: No arguments specified")
 		unless (checkFlags flags)		$ throw (Error "Error: Invalid flag")
 		unless (checkLFlags longFlags)	$ throw (Error "Error: Invalid long flag")
 
@@ -109,4 +117,6 @@ main = do
 		print ("rootDir: " ++ rootDir)
 		print ("arguments: " ++ concat arguments)
         print ("flags: " ++ concat flags)
+        print ("flagArgs: " ++ concat flagArgs) 
         print ("longFlags: " ++ concat longFlags)
+        print ("longFlagArgs: " ++ concat longFlagArgs)
