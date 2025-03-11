7d45db7f791008365c260611ec61e50970642583
Jack Kibort <jkibort928@gmail.com>
2025-03-10 18:43:37 -0700

    request line extracted

--- src/SimpleHttp.hs
+++ src/SimpleHttp.hs
@@ -1,10 +1,10 @@
 module SimpleHttp ( simpleHttpDecode ) where
 
+-- Library Imports
 import Network.Socket ( Socket )
 import Network.Socket.ByteString ( recv, sendAll )
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC ( pack )
-import Control.Monad ( when )
+import qualified Data.ByteString.Char8 as BSC ( pack, unpack )
 
 bufferSize :: Int
 bufferSize = 1024
@@ -62,8 +62,24 @@ readRequest sock = getHeaders BS.empty 0
         -- Function to check if \r\n\r\n is in the buffer
         hasHeaderEnd :: BS.ByteString -> Bool
         hasHeaderEnd buf = BS.isInfixOf (BSC.pack "\r\n\r\n") buf
-                
-            
+
+-- Tail but it can handle null
+tailSafe :: [a] -> [a]
+tailSafe [] = []
+tailSafe xs = tail xs
+
+-- Breaks up the request line by spaces into a triple
+unpackReqLine :: String -> (String, String, String)
+unpackReqLine str = (fst split1, fst split2, tailSafe $ snd split2)
+    where 
+        split1 = break (' '==) str
+        split2 = break (' '==) (tailSafe $ snd split1)
+
+-- Ensures no "naughtiness" in the unix filepath 
+-- Prevents backtracking, and other inappropriate actions that might compromise security
+-- Placeholder implementation
+sanitizePath :: String -> String
+sanitizePath path = path
 
 ---------- Exported -----------
 
@@ -72,9 +88,25 @@ simpleHttpDecode :: Socket -> IO (String, String)
 simpleHttpDecode sock = do
     -- Recieve the request
     request <- readRequest sock
-    putStrLn ( "REQUEST:\n" ++ show request)
+    putStrLn ("FULL REQUEST:\n" ++ show request)
+    putStrLn "----------------------------------"
     
-    -- Decode the header
+    -- Decode the request line
+    let reqLine = BSC.unpack $ fst $ BS.breakSubstring (BSC.pack "\r\n") request
+    putStrLn ("Request line: " ++ reqLine)
+
+    -- Extract Method, URI, and HTTP Version
+    let (method, rawUri, httpVer) = unpackReqLine reqLine
+            
+    putStrLn ("method: " ++ method ++ "\nrawUri: " ++ rawUri ++ "\nhttpVer: " ++ httpVer)
+
+    -- method only GET or HEAD
+
+    -- URI requires leading slash, ignores trailing slash,
+    --and if target is a directory you return index.html of that directory, if no exist then 404
+    --if no index.html, generate one using 'ls' and make an html page that lets you navigate the files and folders
+
+    -- version check 1.1 or 1.0 works
     
     send501 sock
 
